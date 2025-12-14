fcc5969dc0584b769d2fd9ffa3a9e1571cc9c828
lomlashagiorgi-wq <lomlashagiorgi@gmail.com>
2025-12-14 04:16:09 -0800

    commit

---
+++ day 08/homework/hmw8.py
@@ -0,0 +1,17 @@
+name = input("toko")
+surname = input("toko12344321")
+age = input(12)
+
+
+print(name)
+print(surname)
+print(age)
+
+
+
+
+
+
+
+
+
---
+++ dey 10/claswork/cw2.py
@@ -0,0 +1,23 @@
+num1= 6
+num2= 2
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
